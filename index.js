161197dcc42a13c5b3ae3bfdfeafe7c2e751f56a
GSU <gsu1234@mail.ru>
2020-06-07 00:18:26 +0900

    парсинг перебором всех страниц по ссылкам в навигации

--- index.js
+++ index.js
@@ -19,14 +19,17 @@ const main = async () => {
         .option("-u, --url [Site URL*]", "Site URL")
         .option("-o, --output [Output path]", "Output path")
         .option("-f, --fileName [File name]", "File name")
+        .option("-m, --crawlingMode [Crawling mode]", "Crawling mode")
         .parse(process.argv);
 
-    const PARSE_SITE_URL = program.url;
+    const PARSE_SITE_URL    = program.url;
+    const BASE_URL          = program.url;
 
     if (PARSE_SITE_URL) {
 
         if (URL_UTILS.isUrlValid(PARSE_SITE_URL)) {
-            const formatedURL = URL_UTILS.formatToStandard(PARSE_SITE_URL);
+            const formatedURL   = URL_UTILS.formatToStandard(PARSE_SITE_URL);
+            const baseURL       = URL_UTILS.formatToStandard(BASE_URL);
 
             const spinner = ora();
             spinner.start(`Starting parse ${formatedURL}`);
@@ -35,6 +38,9 @@ const main = async () => {
                 const siteName = URL_UTILS.splitURL(formatedURL).authority;
                 const outputPath = program.output || path.join(__dirname, "/output");
                 const fileName = program.fileName || siteName + "_" + getCurrentDate() + ".json";
+                const crawlingMode = program.crawlingMode || 'linear';
+
+                //если crawlingMode = linear , то formatedURL - это стартовая страница (например https://www.php.net/manual/en/copyright.php) , с которой мы начнем листать страницы
 
                 if (isFileAlreadyExist(outputPath, fileName)) {
                     throw new Error(
@@ -44,7 +50,18 @@ const main = async () => {
                         chalk.cyan(outputPath));
                 }
 
-                const parsedData = await parse(formatedURL);
+                let nextPageHref = formatedURL;
+                while (1) {
+                    let parsedData = await parse(nextPageHref, crawlingMode);
+                    if (parsedData.nextPageHref) {
+                        nextPageHref = parsedData.nextPageHref[0];
+                        console.log(nextPageHref);
+
+                    } else {
+                        break;
+                    }
+                }
+                console.log(parsedData);
 
                 await saveFile(outputPath, fileName, JSON.stringify(parsedData));
 
